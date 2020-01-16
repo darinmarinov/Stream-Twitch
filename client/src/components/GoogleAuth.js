e0a84d751abec7449ca19ee093ff73d2bce63457
darinmarinov <darinmarinov@users.noreply.github.com>
2020-01-16 14:02:12 +0200

    Setup Google Auth Api and sign in and sign out  metods

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,19 +2,55 @@ import React from 'react'
 
 class GoogleAuth extends React.Component{
 
+     state ={
+         isSignedIn:null
+     }
+
     componentDidMount(){
-        window.GamepadHapticActuator.load('client:auth2', ()=>{
-            window.GamepadHapticActuator.client.init({
-                clientId:356398052274-chc7dof5bsqbdmjek414scqvvpgqct8m.apps.googleusercontent.com,
-                scope: 'email'
+        window.gapi.load('client:auth2', ()=>{
+            window.gapi.client.init({
+                clientId:"356398052274-chc7dof5bsqbdmjek414scqvvpgqct8m.apps.googleusercontent.com",
+                scope: "email"
+            }).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn: this.auth.isSignedIn.get()})
+                this.auth.isSignedIn.listen(this.onAuthChange)
             })
         })
+    }
+
+    onAuthChange = () => {
+        this.setState({isSignedIn: this.auth.isSignedIn.get()})
+    }
+
+    renderButton = () => {
+        if(this.state.isSignedIn === null){
+            return (
+                <div>I don't know if we are signed In</div>
+            )
+        }
+        else if(this.state.isSignedIn){
+            return(
+               <button className="ui red google button" onClick={()=>this.auth .signOut()}>
+                    <i className="google icon"/>
+                    Sign Out
+               </button>
+            )
+        }
+        else{
+            return (
+                <button className="ui red google button" onClick={()=>this.auth .signIn()}>
+                    <i className="google icon"/>
+                   Sign In
+                </button> 
+            )
+        }
     }
 
     render(){
         return(
            <React.Fragment>
-               <p>GoogleAuth</p>
+               {this.renderButton()}
            </React.Fragment>
         )
     }
