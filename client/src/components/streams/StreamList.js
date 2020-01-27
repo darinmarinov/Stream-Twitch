782f4ea3116e5fa8b89557f7ceb563d07ee767d1
darinmarinov <darinmarinov@users.noreply.github.com>
2020-01-27 16:28:39 +0200

    Add delete and edit button to only register users

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,35 +1,56 @@
 import React from 'react'
-import {connect} from 'react-redux';
-import { fetchStreams} from '../../actions'
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions'
 
 class StreamList extends React.Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.fetchStreams()
     }
-     
-    render(){
-        const {streams} = this.props;
-        console.log(streams)
-        return(
-           <>
-           {
-              streams != 0 || streams.map(stream =>{
-                  return(
-                      <div>{stream.id}</div>
-                  )
-              })
-           }
-           </>
+
+    renderList = () => {
+        return this.props.streams.map((stream) => {
+            return (
+                <div className='item' key={stream.id}>
+                    <i className='icon camera aligned middle' />
+                    <div className='content'>
+                        <h3>{stream.title}</h3>
+                        <div className='description'>
+                            {stream.description}
+                        </div>
+                    </div>
+                    {this.props.uid === stream.userId ?
+                        <div className='buttons'>
+                            <button className="btn waves-effect waves-light" type="submit">Delete</button>
+                            <button className="btn waves-effect waves-light" type="submit">Edit</button>
+                        </div>
+                        :
+                        ''
+                    }
+                </div>
+            )
+        })
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            <>
+                <h2>Stream:</h2>
+                <div className='ui celled list'>
+                    {this.renderList()}
+                </div>
+            </>
         )
     }
 }
 
 const mapStateToProps = state => {
-    return({
-        streams: state.streams 
-    })
+    return {
+        streams: Object.values(state.streams),
+        uid: state.auth.userId
+    }
 }
 
 
-export default connect(mapStateToProps,{fetchStreams})(StreamList)
+export default connect(mapStateToProps, { fetchStreams })(StreamList)
