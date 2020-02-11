e545d10cb016c8af395dbb7aeebbfd6af3fe3985
darinmarinov <darinmarinov@users.noreply.github.com>
2020-02-11 08:17:46 +0200

    Add Stream Delete Component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,13 @@
 import {signIn} from './SignInAction'
 import {signOut} from './SignOutAction'
-import {createStream, fetchStreams, editStream} from './StreamAction'
+import {createStream, fetchStreams, editStream, fetchStream, deleteStream} from './StreamAction'
 
 export {
     signIn,
     signOut,
     createStream,
     fetchStreams,
-    editStream
+    editStream,
+    fetchStream,
+    deleteStream
 }
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,24 +1,50 @@
 import React from 'react'
 import Modal from '../Modal'
+import { fetchStream, deleteStream } from '../../actions'
+import { connect } from 'react-redux'
+import {Link} from 'react-router-dom'
+
+class StreamDelete extends React.Component {
 
 
-const StreamDelete = () => {
-    const actions = (
-        <>
-        <button className='ui button negative'>Delete</button>
-        <button className='ui button'>Cancel</button>
-        </>
-    )
-    return(
-        <div>
-            StreamDelete
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    renderActions = () => {
+        return (
+            <>
+                <button onClick={()=> this.props.deleteStream(this.props.match.params.id)} className='ui button negative'>Delete</button>
+                <Link to="/" className='ui button'>Cancel</Link>
+            </>
+        )
+    }
+
+
+    renderContent() {
+        if (!this.props.stream) {
+            return 'Are you sure you want to delete this stream?'
+        }
+
+        return `Are you sure you want to delete this stream with title ${this.props.stream.title}`
+
+    }
+
+
+    render() {
+
+        return (
             <Modal
-            title={'Delete Stream'}
-            content={'Are you sure you want to Delete this Stream'}
-            actions={actions}
+                title={'Delete Stream'}
+                content={this.renderContent()}
+                actions={this.renderActions()}
             />
-        </div>
-    )
+        )
+    }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete)
